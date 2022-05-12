321bb6d3d99dc909e837a6fddc6d9a4804e90946
Entormix333 <98430259+Entormix333@users.noreply.github.com>
2022-05-12 11:26:08 -0300

    Update UltimateTheory.js

--- UltimateTheory.js
+++ UltimateTheory.js
@@ -35,7 +35,7 @@ var init = () => {
     {
         let getDesc = (level) => "c_2=2^{" + level + "}";
         let getInfo = (level) => "c_2=" + getC2(level).toString(0);
-        c2 = theory.createUpgrade(1, currency, new ExponentialCost(1e10000, Math.log2(1e100)));
+        c2 = theory.createUpgrade(1, currency, new ExponentialCost(1e100, Math.log2(1e100)));
         c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
         c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
     }
